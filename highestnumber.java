f9f114c763210506d7a9f4107aa200da6d1c8d66
starterdragon <papadie102@gmail.com>
2018-08-11 14:43:00 +0800

    Update highestnumber.java

--- highestnumber.java
+++ highestnumber.java
@@ -2,34 +2,32 @@ import java.util.*;
 public class highestnumber
  { 
 	static int[] cache;
- 	static Scanner hn = new Scanner (System.in);
+ 	static Scanner patricia = new Scanner (System.in);
  	
 	public static void main(String args[])
  	{ 
 
 		System.out.println("Please enter how many numbers we're going check:");
-		int n = hn.nextInt();
+		int yel = patricia.nextInt();
 		
-		if (n == 1){
+		if (yel == 1){
 			System.out.println("Please check 2 or more Integers");
 			return;
 		}
 		
-		cache = new int[n];
+		cache = new int[yel];
 		
 		int i = 0;
 		do {
 
 		    System.out.println("Please enter a number:");
-		    int val = hn.nextInt();
+		    int val = patricia.nextInt();
 			cache[i] = val;
 			i++;
 			
-		} while (i != n);
+		} while (i != yel);
 		
 		IntSummaryStatistics stat = Arrays.stream(cache).summaryStatistics();
-        int high = stat.getMax();
-		System.out.println("Highest number is: " + high);
-      
+		System.out.println("Highest number is: " + stat.getMax());
  	}
  }
