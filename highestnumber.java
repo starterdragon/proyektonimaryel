65d4dc481908a0a78e5f163c21bc5175177c63e0
starterdragon <papadie102@gmail.com>
2018-08-11 13:59:34 +0800

    Update highestnumber.java

--- highestnumber.java
+++ highestnumber.java
@@ -10,20 +10,22 @@ public class highestnumber
 		System.out.println("Please enter how many numbers are we checking:");
 		int n = hn.nextInt();
 		
-		if (n != (int) n || n == (int) 1){
-			System.out.println("Wrong input, please try again");
+		if (n == 1){
+			System.out.println("Please check 2 or more Integers");
 			return;
 		}
 		
 		cache = new int[n];
 		
 		int i = 0;
-		do{
+		do {
+
 		    System.out.println("Please a number:");
 		    int val = hn.nextInt();
 			cache[i] = val;
 			i++;
-		}while (i != n);
+			
+		} while (i != n);
 		
 		IntSummaryStatistics stat = Arrays.stream(cache).summaryStatistics();
         int high = stat.getMax();
