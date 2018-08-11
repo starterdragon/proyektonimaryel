ec0e8ebbbd71dd5166ef42a18d646987ad70dee4
starterdragon <papadie102@gmail.com>
2018-08-11 13:12:05 +0800

    Create Oddoreven.java

---
+++ Oddoreven.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+
+
+
+ /*Please dont change class name, Dcoder 
+ and class must not be public*/
+
+
+ //Compiler version JDK 1.8
+
+
+
+
+public class Oddoreven
+ { 
+ 	static int eoro;
+ 	static Scanner odd=new Scanner (System.in);
+ 	
+	public static void main(String args[])
+ 	{ 
+
+
+		System.out.println("Input a number: ");
+		eoro=odd.nextInt();
+		
+		if (eoro%2==0){
+			System.out.println("Number is even ");
+		}
+		else {
+			System.out.println("Number is odd ");
+		}
+			
+		
+
+
+ 	}
+ }
