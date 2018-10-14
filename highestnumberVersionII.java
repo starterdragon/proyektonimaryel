eeea75190ad9e0b94400c0f208db8d08286b07dd
starterdragon <papadie102@gmail.com>
2018-10-14 17:18:57 +0800

    Update highestnumberVersionII.java

--- highestnumberVersionII.java
+++ highestnumberVersionII.java
@@ -9,24 +9,22 @@ public class highestnumberVersionII
  	}
  	
  	public static void actual( Scanner patricia ){
- 	    System.out.println("Please enter how many numbers we're going check:");
+ 	    System.out.println("Please enter how many numbers we're gonna check:");
 		int yel = patricia.nextInt();
 		if (yel == 1){
-			System.out.println("Please check 2 or more Integers");
+			System.out.println("Please check two or more integers");
 			return;
 		}
 		
 		cache = new int[yel];
-		
 		System.out.println("Please enter "+ yel +" numbers:");
 		
 		int i = 0;
+		
 		do {
-
 		    int val = patricia.nextInt();
 			cache[i] = val;
 			i++;
-			
 		} while (i != yel);
 		
 		IntSummaryStatistics stat = Arrays.stream(cache).summaryStatistics();
